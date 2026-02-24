819f96de8239d6ad33a0bc16dde81fd63aaecacb
alasiri <austinalasiri44@gmail.com>
2026-02-24 06:36:40 +0300

    updated table name

--- customers/models.py
+++ customers/models.py
@@ -2,7 +2,7 @@ from core.models import BaseModel
 from core.managers import ActiveManager, AllObjectsManager
 from django.db import models
 
-class Branch(BaseModel):
+class Customers(BaseModel):
     name = models.CharField(max_length=255)
     
     objects = ActiveManager()
