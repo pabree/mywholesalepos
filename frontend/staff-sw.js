8332b3b822f7f162a13ff9f4b49299838133982c
pabree <pauljumah12@gmail.com>
2026-04-13 17:11:45 +0300

    still tweaking

--- frontend/staff-sw.js
+++ frontend/staff-sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "staff-app-v43";
-const APP_BUILD = "2026-04-13.5";
+const CACHE_NAME = "staff-app-v44";
+const APP_BUILD = "2026-04-13.6";
 const APP_SHELL = [
     "/",
     `/static/style.css?v=${APP_BUILD}`,
